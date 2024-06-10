2f7352f9f6a7e7aa579bebb675206d6824cabbca
Arne Weber <72623832+Windows200000@users.noreply.github.com>
2024-06-10 16:28:35 +0200

    Added script

---
+++ Matrices.py
@@ -0,0 +1,69 @@
+import json
+import copy
+from fractions import Fraction
+import shutil
+import math
+
+size = int(input("size: "))
+Default = []
+for row in range(size):
+  Default.append([])
+  for column in range(size):
+    Default[row].append(0)
+  for column in range(size):
+    if row == column:
+      Default[row].append(1)
+    else:
+      Default[row].append(0)
+Current = copy.deepcopy(Default)
+
+
+def draw(text):
+  max_length = shutil.get_terminal_size().columns
+  justify = math.floor((max_length - 2*size)/(2*size))
+  
+  print(chr(27) + "[2J")
+  for line in Current:
+    pretty = ""
+    for i, frac in enumerate(line):
+      pretty = pretty + str(frac).ljust(justify)
+      if i + 1 == size:
+        pretty = pretty + '|'
+      else:
+        pretty = pretty + ','
+    print(pretty)
+  print (text)
+
+while True:
+  draw("clear(cl), define(def), line(number):")
+  cli = input()
+  match cli:
+    case "clear" | "cl":
+      Current = copy.deepcopy(Default)
+      #break
+
+    case "define" | "def":
+      for line, line_v in enumerate(Current):
+        for column, column_v in enumerate(line_v[:len(line_v)//2]):
+          draw(f"({line+1}|{column+1})")
+          val = input()
+          if val == '':
+            val = Current[line][column]
+          else:
+            val = Fraction(val)
+          Current[line][column] = val
+      #break
+
+    case source:
+      source = int(source) - 1
+      text = f"Source line = {int(source + 1)}, Factor"
+      draw(f"{text}:")
+      factor = Fraction(input())
+      text += f" = {factor}, Target line"
+      draw(f"{text}:") 
+      target = int(input()) - 1
+      for cell, cellv in enumerate(Current[source]):
+        if not target == source:
+          Current[target][cell] = Current[target][cell]+cellv*factor
+        else:
+          Current[target][cell] = cellv*factor
